3479623c9bf3c62e3245411216247c364b0a9ae4
Sarim <105370198+Sksarim@users.noreply.github.com>
2025-05-21 03:50:51 +0000

    ecom-firstapi

---
+++ my-app/src/components/Contact.jsx
@@ -0,0 +1,9 @@
+import Base from './Base';
+export default function Contact() {
+    return (
+        <>
+            <Base />
+            <h1 className="text-center">Contact Page</h1>
+        </>
+    )
+}
